8a16b1834edf3da95ec1b635d24150a9f334e2fa
Nhathv21 <Nhathv.21it@gmail.com>
2024-01-20 23:27:55 +0700

    Install mongoose, connect and setup models

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 require('dotenv').config()
 const router = require("./routes/client/index.router")
+const database = require("./config/database")
+
 const app = express()
 const port = process.env.PORT
-
+database.connect();
 app.set("vỉews","./views")
 app.set('view engine', 'pug')
 app.use(express.static("public"))
