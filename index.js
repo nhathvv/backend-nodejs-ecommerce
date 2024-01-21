80c1be7a9a509c8209fdc187ae273a97ffc8cf58
Nhathv21 <Nhathv.21it@gmail.com>
2024-01-21 10:49:44 +0700

    Create admin dashboard, set up router

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('dotenv').config()
+const routerAdmin = require("./routes/admin/index.router")
 const router = require("./routes/client/index.router")
 const database = require("./config/database")
 
@@ -11,7 +12,9 @@ app.set('view engine', 'pug')
 app.use(express.static("public"))
 
 // Router
+routerAdmin(app)
 router(app)
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
---
+++ routes/admin/dashboard.router.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+const controllers = require("../../controllers/admin/dashboard.controller")
+
+router.get('/', controllers.dashboard);
+module.exports = router;
