e50d5b48c0f813f35be8aa04c34161b248be43a7
Nhathv21 <Nhathv.21it@gmail.com>
2024-01-31 16:55:14 +0700

    feat: (upload image) create product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -92,6 +92,7 @@ const create = async(req,res) =>{
 }
 // [POST] /admin/products/create
 const createProduct = async(req,res) => {
+    console.log(req.file)
     req.body.price = parseInt(req.body.price)
     req.body.discountPercentage = parseInt(req.body.discountPercentage)
     req.body.stock = parseInt(req.body.stock)
@@ -102,6 +103,8 @@ const createProduct = async(req,res) => {
     }else {
         req.body.position = parseInt(req.body.position);
     }
+    req.body.thumbnail = `/uploads/${req.file.filename}`
+    
     const product = new Product(req.body);
     await product.save();
 
--- routes/admin/product.router.js
+++ routes/admin/product.router.js
@@ -1,12 +1,16 @@
 const express = require('express')
-const router = express.Router()
+const storageMulter = require("../../helpers/storageMulter")
 const controllers = require("../../controllers/admin/product.controller")
+const router = express.Router()
+const multer  = require('multer')
+
+const upload = multer({storage :storageMulter()})
 
 router.get('/', controllers.index);
 router.patch('/change-status/:status/:id', controllers.changeStatus);
 router.patch('/change-multi', controllers.changeMulti);
 router.delete('/delete/:id', controllers.deleteItem);
 router.get("/create", controllers.create)
-router.post("/create", controllers.createProduct);
+router.post("/create",upload.single('thumbnail'),controllers.createProduct);
 
 module.exports = router;
