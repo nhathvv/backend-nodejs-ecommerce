a0f4b282b6b3891fcca9f634acc8d3eeae1c040a
Nhathv21 <Nhathv.21it@gmail.com>
2024-02-25 18:11:10 +0700

    feat: push static files to the cloud

--- routes/admin/product.router.js
+++ routes/admin/product.router.js
@@ -1,18 +1,20 @@
 const express = require('express')
-const storageMulter = require("../../helpers/storageMulter")
 const controllers = require("../../controllers/admin/product.controller")
 const validates = require("../../validates/admin/product.validate")
 const router = express.Router()
 const multer  = require('multer')
-
-const upload = multer({storage :storageMulter()})
-
+const upload = multer()
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware")
 router.get('/', controllers.index);
 router.patch('/change-status/:status/:id', controllers.changeStatus);
 router.patch('/change-multi', controllers.changeMulti);
 router.delete('/delete/:id', controllers.deleteItem);
 router.get("/create", controllers.create)
-router.post("/create",upload.single('thumbnail'),validates.createPost,controllers.createProduct);
+router.post("/create",
+        upload.single('thumbnail'),
+        validates.createPost,
+        uploadCloud.upload,
+        controllers.createProduct);
 router.get("/edit/:id", controllers.edit)
 router.patch("/edit/:id",upload.single('thumbnail'),validates.createPost,controllers.editProduct);
 router.get("/detail/:id",controllers.detail)
