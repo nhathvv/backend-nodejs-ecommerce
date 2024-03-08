5e9d414a7e3463ecd9835ce76de0258d1954ead7
Nhathv21 <Nhathv.21it@gmail.com>
2024-03-08 11:13:45 +0700

    feat: display featured products client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,7 +1,18 @@
+const Product = require("../../models/product.model")
+const productHelper = require("../../helpers/product")
 // [GET] : /
-const index = (req, res) => {
+const index = async (req, res) => {
+    // Featured products
+    let find = {
+        featured : "1",
+        status : "active",
+        deleted : false
+    }
+    const products = await Product.find(find).limit(6)
+    const productFeatured = productHelper.priceNewProducts(products)
     res.render("client/pages/home/index", {
-        pageTitle : "Trang chủ"
+        pageTitle : "Trang chủ",
+        productFeatured : productFeatured,
     })
 }
 module.exports = {
